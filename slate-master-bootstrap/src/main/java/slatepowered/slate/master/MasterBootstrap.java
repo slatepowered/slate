89040aa168fd922fa4ed21a0adfde8b25dd402c6
orbyfied <atomf88zero@gmail.com>
2023-11-16 00:54:34 +0100

    its 1 am

--- slate-master-bootstrap/src/main/java/slatepowered/slate/master/MasterBootstrap.java
+++ slate-master-bootstrap/src/main/java/slatepowered/slate/master/MasterBootstrap.java
@@ -3,7 +3,10 @@ package slatepowered.slate.master;
 import slatepowered.slate.logging.JavaLoggerProvider;
 import slatepowered.slate.logging.Logger;
 import slatepowered.slate.logging.Logging;
+import slatepowered.veru.config.Configuration;
+import slatepowered.veru.config.YamlConfigParser;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
@@ -11,6 +14,8 @@ import java.nio.file.Paths;
  */
 public class MasterBootstrap {
 
+    private static final Path CONFIG_PATH = Paths.get("./config.yml");
+    private static final String CONFIG_DEFAULTS = "/config.defaults.yml";
     private static final Logger LOGGER = Logging.getLogger("MasterBootstrap");
 
     public static void main(String[] args) throws Throwable {
@@ -21,6 +26,12 @@ public class MasterBootstrap {
         // todo: parse config and options
         //  make options replace config values
         //  then actually start the Master instance
+
+        // load init configuration
+        Configuration config = new Configuration().withParser(YamlConfigParser.standard());
+        config.reloadOrDefaultThrowing(CONFIG_PATH, CONFIG_DEFAULTS);
+
+        // parse args (config overrides)
     }
 
 }
